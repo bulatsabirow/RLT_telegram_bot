9f7b30735b9282b3338d2e1bfc35220d34ca7a6d
bulatsabirow <bulatsabirow@gmail.com>
2024-04-18 12:37:15 +0300

    + logging

---
+++ logger.py
@@ -0,0 +1,8 @@
+import logging
+import sys
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(logging.Formatter(fmt="%(asctime)s-%(levelname)s-%(message)s"))
+logger.addHandler(handler)
--- services/bot/bot_handlers.py
+++ services/bot/bot_handlers.py
@@ -4,12 +4,14 @@ from telegram.ext import ContextTypes
 
 from db import collection
 from db.mongo import MongoDBService
+from logger import logger
 from services.handlers import TelegramDataHandler
 
 mongo_service = MongoDBService(collection)
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    logger.info(f"Received start command from {update.effective_user}")
     user = update.effective_user
     await update.message.reply_text(
         f"Hi {user.mention_markdown_v2()}!", parse_mode=ParseMode.MARKDOWN
@@ -19,11 +21,14 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 async def send_salary_aggregation_data(
     update: Update, context: ContextTypes.DEFAULT_TYPE
 ) -> None:
+    logger.info(
+        f"Received message about salary aggregation data: {update.message.text}\n"
+        f" from {update.effective_user}"
+    )
     try:
         data_handler = TelegramDataHandler(update.message.text, mongo_service)
     except ValueError as exc:
         response = exc.args[0]
     else:
         response = str(await data_handler.serialize_response())
-
     await update.message.reply_text(response)
